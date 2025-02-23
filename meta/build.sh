e7a6ffa4de8c6d9f0a1434cd4c10862677471658
Mehran Kamal <29648490+mehrankamal@users.noreply.github.com>
2025-02-23 19:15:44 +0500

    Setup raylib for rendering web content.

--- meta/build.sh
+++ meta/build.sh
@@ -1,5 +1,22 @@
-#! /bin/bash
+#!/bin/bash
 
-cmake -B build -S . -G Ninja
-pushd build
-ninja
+BUILD_DIRECTORY="build/cmake"
+VCPKG_DIR="build/toolchain/vcpkg"
+
+ensure_vcpkg_installation() {
+    if [ ! -d "$VCPKG_DIR" ]; then
+        echo "Installing vcpkg in $VCPKG_DIR..."
+        git clone https://github.com/Microsoft/vcpkg.git "$VCPKG_DIR"
+        (cd "$VCPKG_DIR" && ./bootstrap-vcpkg.sh -disableMetrics)
+    fi
+}
+
+mkdir -p "$BUILD_DIRECTORY"
+
+ensure_vcpkg_installation
+
+cmake -B "$BUILD_DIRECTORY" -S . -G Ninja \
+    -DCMAKE_TOOLCHAIN_FILE="$VCPKG_DIR/scripts/buildsystems/vcpkg.cmake" \
+    -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+
+cmake --build "$BUILD_DIRECTORY"
--- src/main.cc
+++ src/main.cc
@@ -1,20 +1,48 @@
-#include <iostream>
+// #include <iostream>
 
-#include "url-parser.h"
-#include "util.h"
+// #include "url-parser.h"
+// #include "util.h"
 
-int main(int argc, char **argv) {
-  if (argc > 1) {
-    MK::UrlParser url_parser = MK::UrlParser(argv[1]);
-    auto maybe_url = url_parser.parse();
+// int main(int argc, char **argv) {
+//   if (argc > 1) {
+//     MK::UrlParser url_parser = MK::UrlParser(argv[1]);
+//     auto maybe_url = url_parser.parse();
 
-    if (maybe_url.has_value()) {
-      MK::load(maybe_url.value());
-    } else {
-      std::cerr << "Error parsing URL" << std::endl;
-    }
-  } else {
-    std::cerr << "Exected URL, none found" << std::endl;
+//     if (maybe_url.has_value()) {
+//       MK::load(maybe_url.value());
+//     } else {
+//       std::cerr << "Error parsing URL" << std::endl;
+//     }
+//   } else {
+//     std::cerr << "Exected URL, none found" << std::endl;
+//   }
+//   return 0;
+// }
+
+#include <raylib.h>
+
+int screenWidth = 800;
+int screenHeight = 450;
+
+void UpdateDrawFrame(void);
+int main() {
+  InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+
+  SetTargetFPS(60);
+  while (!WindowShouldClose()) {
+    UpdateDrawFrame();
   }
+
+  CloseWindow();
+
   return 0;
+}
+void UpdateDrawFrame(void) {
+  BeginDrawing();
+
+  ClearBackground(RAYWHITE);
+
+  DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+
+  EndDrawing();
 }
