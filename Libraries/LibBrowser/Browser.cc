c0560e080d18054d138d45935e8fc25ff03b9477
Mehran Kamal <29648490+mehrankamal@users.noreply.github.com>
2025-03-21 16:11:18 +0500

    LibBrowser: Add some custom font rendering capabilities

--- Libraries/LibBrowser/Browser.cc
+++ Libraries/LibBrowser/Browser.cc
@@ -1,15 +1,16 @@
+#include <cassert>
 #include <iostream>
-#include <string>
-
 #include <raylib.h>
 #include <raymath.h>
+#include <string>
+#include <vector>
 
 #include "Browser.hh"
 #include "Forward.hh"
 #include "Util.hh"
 
-#define HSTEP 13u
-#define VSTEP 18u
+#define HSTEP 13.0f
+#define VSTEP 18.0f
 #define SCROLL_STEP 100u
 
 namespace MK {
@@ -17,6 +18,7 @@ Browser::Browser()
 {
     InitWindow(WIDTH, HEIGHT, "MK Browser");
     SetTargetFPS(60);
+    m_font = LoadFont("resources/fonts/SF-Pro-Text-Regular.ttf");
 }
 
 void Browser::load(URL const& url)
@@ -26,30 +28,52 @@ void Browser::load(URL const& url)
     compute_layout(m_text_content);
 }
 
+static std::vector<std::string> split_to_chunks(std::string const& text_content)
+{
+    std::vector<std::string> chunks;
+    std::string current_chunk;
+
+    for (char c : text_content) {
+        if (c == ' ' || c == '\n') {
+            if (!current_chunk.empty()) {
+                chunks.push_back(current_chunk);
+                current_chunk.clear();
+            }
+        } else {
+            current_chunk += c;
+        }
+    }
+
+    if (!current_chunk.empty()) {
+        chunks.push_back(current_chunk);
+    }
+
+    return chunks;
+}
+
 void Browser::compute_layout(std::string const& text_content)
 {
     auto cursor_x = HSTEP;
     auto cursor_y = VSTEP;
 
-    for (size_t i = 0; i < text_content.length(); ++i) {
-        if (text_content[i] == '\n') {
-            cursor_x = HSTEP;
-            cursor_y += VSTEP;
-            continue;
-        }
+    for (auto chunk : split_to_chunks(text_content)) {
+        assert(chunk.find(" ") == std::string::npos);
+
+        auto width
+            = MeasureTextEx(m_font, chunk.c_str(), m_font_size, m_spacing).x;
 
         LayoutText layout_text = {
-            .position
-            = { static_cast<float>(cursor_x), static_cast<float>(cursor_y) },
-            .character = std::string(1, text_content[i]),
+            .position = { cursor_x, cursor_y },
+            .character = chunk,
         };
 
         m_display_list.push_back(layout_text);
-        cursor_x += HSTEP;
+        cursor_x
+            += width + MeasureTextEx(m_font, " ", m_font_size, m_spacing).x;
 
-        if (cursor_x >= WIDTH - HSTEP) {
+        if (cursor_x + width >= WIDTH - HSTEP) {
             cursor_x = HSTEP;
-            cursor_y += VSTEP;
+            // cursor_y += (m_font.baseSize + m_font.glyphPadding) * 1.25;
         }
     }
 }
@@ -64,13 +88,13 @@ void Browser::draw_layout() const
             continue;
 
         DrawTextEx(m_font, layout_text.character.c_str(),
-            Vector2Add(layout_text.position, { 0, -m_scroll }), 13, 0, BLACK);
+            Vector2Add(layout_text.position, { 0, -m_scroll }), m_font_size,
+            m_spacing, BLACK);
     }
 }
 
 void Browser::run()
 {
-    m_font = LoadFont("resources/fonts/SF-Pro-Text-Regular.ttf");
     while (!WindowShouldClose()) {
         update_draw_frame();
     }
@@ -92,13 +116,9 @@ void Browser::update_state()
 void Browser::update_draw_frame()
 {
     update_state();
-
     BeginDrawing();
-
     ClearBackground(RAYWHITE);
-
     draw_layout();
-
     EndDrawing();
 }
 
--- Libraries/LibBrowser/Browser.hh
+++ Libraries/LibBrowser/Browser.hh
@@ -8,27 +8,29 @@
 namespace MK {
 class Browser {
 public:
-  struct LayoutText {
-    Vector2 position;
-    std::string character;
-  };
+    struct LayoutText {
+        Vector2 position;
+        std::string character;
+    };
 
-  Browser();
-  void load(URL const &url);
-  void run();
+    Browser();
+    void load(URL const& url);
+    void run();
 
-  std::string const &text_content() const { return m_text_content; }
+    std::string const& text_content() const { return m_text_content; }
 
 private:
-  void update_draw_frame();
-  void close_window() const;
-  void compute_layout(std::string const &text_content);
-  void draw_layout() const;
-  void update_state();
+    void update_draw_frame();
+    void close_window() const;
+    void compute_layout(std::string const& text_content);
+    void draw_layout() const;
+    void update_state();
 
-  std::vector<LayoutText> m_display_list;
-  std::string m_text_content;
-  Font m_font;
-  float m_scroll = 0.0f;
+    std::vector<LayoutText> m_display_list;
+    std::string m_text_content;
+    Font m_font;
+    float m_scroll { 0.0f };
+    float m_font_size { 13.0f };
+    float m_spacing { 0.0f };
 };
 } // namespace MK
